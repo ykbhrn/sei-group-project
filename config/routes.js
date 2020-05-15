366ac2842564b270ad9700654fd5930564aa869e
George Jones <george@circleaudiovisual.com>
2020-05-15 15:37:21 +0100

    added scientific name search to new plant form

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const plants = require('../controllers/plants')
 const auth = require('../controllers/auth')
+const apiProxies = require('../controllers/apiProxies')
 
 
 router.route('/plants')
@@ -21,5 +22,8 @@ router.route('/register')
 
 router.route('/login')
   .post(auth.login)
+
+router.route('/trefle')
+  .post(apiProxies.getTrefleInfo)
 
 module.exports = router
--- frontend/src/components/plants/NewPlant.js
+++ frontend/src/components/plants/NewPlant.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import Select from 'react-select'
 import { newPlant } from '../../lib/api'
 
 class NewPlant extends React.Component {
@@ -9,29 +11,51 @@ class NewPlant extends React.Component {
       description: '',
       imageUrl: '',
       height: ''
-    }
+    },
+    options: []
   }
 
   handleChange = event => {
     const formData = { ...this.state.formData, [event.target.name]: event.target.value }
-    this.setState( { formData } )
+    console.log(this.state.formData)
+    this.setState({ formData })
   }
 
   handleSubmit = async event => {
     try {
-       await newPlant(this.state.formData)
+      event.preventDefault()
+      await newPlant(this.state.formData)
       // this.props.history.push(`/plants`)
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   }
+
+  searchForScientific = async () => {
+    // console.log('running with: ', this.state.formData.name)
+    const searchQuery = this.state.formData.name
+    const response = await axios.post('/api/trefle', { search: `${searchQuery}` })
+    // console.log('response is: ', response.data)
+    this.populateOptions(response.data)
+  }
+
+  populateOptions = async (plantData) => {
+    const scientificNames = []
+    plantData.forEach(plant => (
+      scientificNames.push({ value: plant.scientific_name, label: plant.scientific_name })
+    ))
+    this.setState({ options: scientificNames })
+    // console.log(this.state.options)
+  }
+
+
 
   render() {
     return (
       <section className="section">
         <div className="container">
-        <form onSubmit={this.handleSubmit} className="column is half is-offset-one-quarter box">
-        <input className="input"
+          <form onSubmit={this.handleSubmit} className="column is half is-offset-one-quarter box">
+            <input className="input"
               placeholder="name"
               name="name"
               onChange={this.handleChange}
@@ -40,9 +64,21 @@ class NewPlant extends React.Component {
             <input className="input"
               placeholder="description"
               name="description"
+
               onChange={this.handleChange}
               value={this.state.formData.description}
             />
+            <div className="field">
+              <div className="control"
+                onClick={this.searchForScientific}
+              >
+                <Select
+                  options={this.state.options}
+                  onChange={this.handleMultiChange}
+                  placeholder="Scientific Name"
+                />
+              </div>
+            </div>
             <input className="input"
               placeholder="Url of your plant image"
               name="imageUrl"
@@ -55,10 +91,9 @@ class NewPlant extends React.Component {
               onChange={this.handleChange}
               value={this.state.formData.height}
             />
-            <button type="submit">
-              Add Plant
-            </button>
-            
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
           </form>
         </div>
 
