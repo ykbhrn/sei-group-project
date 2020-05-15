7824d36f6c2f8a43a883ac2e8e1c062e555ab8fd
ykbhrn <jakub.horun@gmail.com>
2020-05-15 13:26:57 +0100

    trade

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
 const plants = require('../controllers/plants')
 const auth = require('../controllers/auth')
+const user = require('../controllers/users')
+const secureRoute = require('../lib/secureRoute')
 
 
 router.route('/plants')
   .get(plants.index)
-  .post(plants.create)
+  .post(secureRoute, plants.create)
 
 
 router.route('/plants/:id')
@@ -14,8 +16,13 @@ router.route('/plants/:id')
   .delete(plants.delete)
 
 
-
 router.route('/register')
   .post(auth.register)
+
+router.route('/login')
+  .post(auth.login)
+
+router.route('/profile')
+  .get(secureRoute, user.profile)
 
 module.exports = router
---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+const User = require('../models/user')
+
+async function userProfile(req, res, next) {
+  try {
+    const user = await User.findById(req.currentUser._id).populate('createdPlants')
+    if (!user) throw new Error({ message: 'Not found' })
+    res.status(200).json(user)
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  profile: userProfile
+}
