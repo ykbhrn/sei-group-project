88053cfd390079b70ca7245d8b89e5a5a9beaae3
George Jones <george@circleaudiovisual.com>
2020-05-15 15:45:24 +0100

    fixed merge conflict

--- frontend/src/components/plants/NewPlant.js
+++ frontend/src/components/plants/NewPlant.js
@@ -23,10 +23,7 @@ class NewPlant extends React.Component {
 
   handleSubmit = async event => {
     try {
-<<<<<<< HEAD
       event.preventDefault()
-=======
->>>>>>> development
       await newPlant(this.state.formData)
       // this.props.history.push(`/plants`)
     } catch (err) {
