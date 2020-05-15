d4b23265c723857bb1addb8cd0b1a53780c3dbe0
ainokyto <aino.kyto@gmail.com>
2020-05-15 19:14:06 +0300

    appended user to each db plant

--- db/data/users.js
+++ db/data/users.js
@@ -3,24 +3,32 @@ module.exports = [
     name: 'Aino',
     email: 'aino@email',
     password: 'pass',
-    passwordConfirmation: 'pass'
+    passwordConfirmation: 'pass',
+    lat: 51.529163,
+    lon: -0.10997
   },
   {
     name: 'George',
     email: 'george@email',
     password: 'pass',
-    passwordConfirmation: 'pass'
+    passwordConfirmation: 'pass',
+    lat: 51.529163,
+    lon: -0.10997
   },
   {
     name: 'Jakub',
     email: 'jakub@email',
     password: 'pass',
-    passwordConfirmation: 'pass'
+    passwordConfirmation: 'pass',
+    lat: 51.529163,
+    lon: -0.10997
   },
   {
     name: 'Yarden',
     email: 'yarden@email',
     password: 'pass',
-    passwordConfirmation: 'pass'
+    passwordConfirmation: 'pass',
+    lat: 51.529163,
+    lon: -0.10997
   }
 ]
