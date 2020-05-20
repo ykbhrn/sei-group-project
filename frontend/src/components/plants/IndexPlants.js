1813b3e321a9167717e8538e792d5b5d977fed0d
ainokyto <aino.kyto@gmail.com>
2020-05-21 01:38:54 +0300

    added hot plants thumbnail to indexplant

--- frontend/src/components/plants/IndexPlants.js
+++ frontend/src/components/plants/IndexPlants.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { getAllPlants } from '../../lib/api'
 import CardPlant from './CardPlant'
+import IndexMapThumb from '../common/IndexMapThumb'
+import Notifications, {notify} from 'react-notify-toast'
 
 class IndexPlants extends React.Component {
   state = {
@@ -11,7 +13,11 @@ class IndexPlants extends React.Component {
     try {
       const res = await getAllPlants()
       console.log(res.data)
-      this.setState({ plants: res.data })
+      const plants = res.data
+      this.setState({ plants })
+
+      const toastColor = { background: '#3FC008', text: 'white' }
+      notify.show(`There are ${plants.length} Hot Plants in Your Area!`, 'custom', 5000, toastColor)
     } catch (err) {
       console.log(err)
     }
@@ -19,15 +25,22 @@ class IndexPlants extends React.Component {
 
   render() {
     return (
+      <>
+      <div className="section">
+        <Notifications />
+          <IndexMapThumb plants={this.state.plants} />
       <section className="section">
         <div className="container">
           <div className="columns is-multiline">
+
             {this.state.plants.map(plant => ( // * Looping over each plant in the array, and producing a CardPlant component for each one, passing each one props of an induvidual cheese.
               <CardPlant key={plant._id} {...plant} />
             ))}
           </div>
         </div>
       </section>
+      </div>
+      </>
     )
   }
 
