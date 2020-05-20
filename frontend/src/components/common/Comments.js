4cf1cdf22c0cb63ef855caea0f34d9d1fb7f8313
YardenLawson <yardenlawson@gmail.com>
2020-05-20 19:42:45 +0100

    update

--- frontend/src/components/common/Comments.js
+++ frontend/src/components/common/Comments.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { addComment, getSinglePlant } from '../../lib/api'
+import { addComment, getSinglePlant, deleteComment } from '../../lib/api'
+import Likes from '../common/Likes'
+import { isOwner } from '../../lib/auth'
 
 class Comments extends React.Component {
   state = {
@@ -8,7 +10,7 @@ class Comments extends React.Component {
     text: ''
   }
 
-  async getData() { //* this function can be called whenever you need to update the info on the page  
+  async getData() { //* this function can be called whenever you need to update the info on the page
     try {
       const plantId = this.props.plantId
       const res = await getSinglePlant(plantId)
@@ -19,16 +21,16 @@ class Comments extends React.Component {
       // this.props.history.push('/notfound')
     }
   }
-  componentDidMount() { 
+  componentDidMount() {
     // console.log(this.props.plantId)
-    this.getData() //* calling the getData function as soon as the page loads to display the info straight away 
+    this.getData() //* calling the getData function as soon as the page loads to display the info straight away
   }
 
-  
+
 //* COMMENTS
 commentHandleChange = event => {
   const text = event.target.value //* saving what the user types into the comment box
-  this.setState({ text }) //* setting state with their comment 
+  this.setState({ text }) //* setting state with their comment
   }
 
 commentHandleSubmit = async event => {
@@ -43,35 +45,69 @@ try {
 this.getData() //* calling this getData function again to reload the page with the new database info and display your new comment straight away!
 }
 
+commentHandleDelete = async event => {
+  event.preventDefault()
+
+  try {
+    const commentId = event.target.getAttribute('comment-id')
+    const plantId = this.state.plant._id
+    await deleteComment(plantId, commentId)
+  } catch (err) {
+    console.log(err)
+  }
+  this.getData()
+}
+
 render() {
   if (!this.state.plant) return null
   const { plant, text } = this.state //* text field in state
-  console.log(text)
+
   return (
-    <div>
-    <p>
-    {plant.comments.map(comment => {
-      return (
-        <p key={comment._id}> {comment.text} </p>
-      )
-    })}
-    </p>
-<form onSubmit={this.commentHandleSubmit}> 
-    <textarea 
+    <div className="media-content">
+<form onSubmit={this.commentHandleSubmit}>
+  <div className="field">
+    <p className="control">
+    <textarea
   className="textarea"
   placeholder="Enter your comment here"
   name="text"
   onChange={this.commentHandleChange}
-  value={text} 
+  value={text}
 />
-{/* the value is now what ever text in state is */}
-<button type="submit" className="comment-submit-button">Add Comment</button>
+</p>
+</div>
+<div className="field">
+<p className="control">
+<button type="submit" className="button">Post Comment</button>
+</p>
+<br />
+</div>
 </form>
+<div>
+    {plant.comments.map(comment => {
+      // console.log(comment)
+      return (
+        <article className="media">
+        <div className="media-content">
+          <div className="content">
+            <div>
+        <strong>{comment.user.name}</strong>
+        <p key={comment._id.toString()}> {comment.text} </p>
+        {isOwner(comment.user._id) &&
+        <button className="button" comment-id={comment._id}
+        onClick={this.commentHandleDelete}>Delete
+        </button>}
+        </div>
+        </div>
+        </div>
+        </article>
+      )
+    })}
+    </div>
 </div>
   )
 }
 
 }
 
-export default Comments 
-
+export default Comments
