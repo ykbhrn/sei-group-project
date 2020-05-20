7a146a86b43aaf17fdf3b5ebbb72f5e184845b15
YardenLawson <yardenlawson@gmail.com>
2020-05-20 23:23:33 +0100

    update

--- frontend/src/components/common/Comments.js
+++ frontend/src/components/common/Comments.js
@@ -27,86 +27,90 @@ class Comments extends React.Component {
   }
 
 
-//* COMMENTS
-commentHandleChange = event => {
-  const text = event.target.value //* saving what the user types into the comment box
-  this.setState({ text }) //* setting state with their comment
+  //* COMMENTS
+  commentHandleChange = event => {
+    const text = event.target.value //* saving what the user types into the comment box
+    this.setState({ text }) //* setting state with their comment
   }
 
-commentHandleSubmit = async event => {
-event.preventDefault()
-const plantId = this.props.plantId
-try {
-  await addComment({text: this.state.text}, plantId) //* the add comment function requires a text field so you can pass it through like so - also it needs to match the order that you're using the arguments in your api.js file
-  this.setState({ text: '' }) //* setting the comment box back to empty
-} catch(err) {
-  console.log(err.response.data)
-}
-this.getData() //* calling this getData function again to reload the page with the new database info and display your new comment straight away!
-}
-
-commentHandleDelete = async event => {
-  event.preventDefault()
-
-  try {
-    const commentId = event.target.getAttribute('comment-id')
-    const plantId = this.state.plant._id
-    await deleteComment(plantId, commentId)
-  } catch (err) {
-    console.log(err)
+  commentHandleSubmit = async event => {
+    event.preventDefault()
+    const plantId = this.props.plantId
+    try {
+      await addComment({ text: this.state.text }, plantId) //* the add comment function requires a text field so you can pass it through like so - also it needs to match the order that you're using the arguments in your api.js file
+      this.setState({ text: '' }) //* setting the comment box back to empty
+    } catch (err) {
+      console.log(err.response.data)
+    }
+    this.getData() //* calling this getData function again to reload the page with the new database info and display your new comment straight away!
   }
-  this.getData()
-}
 
-render() {
-  if (!this.state.plant) return null
-  const { plant, text } = this.state //* text field in state
+  commentHandleDelete = async event => {
+    event.preventDefault()
 
-  return (
-    <div className="media-content">
-<form onSubmit={this.commentHandleSubmit}>
-  <div className="field">
-    <p className="control">
-    <textarea
-  className="textarea"
-  placeholder="Enter your comment here"
-  name="text"
-  onChange={this.commentHandleChange}
-  value={text}
-/>
-</p>
-</div>
-<div className="field">
-<p className="control">
-<button type="submit" className="button">Post Comment</button>
-</p>
-<br />
-</div>
-</form>
-<div>
-    {plant.comments.map(comment => {
-      // console.log(comment)
-      return (
-        <article className="media">
-        <div className="media-content">
-          <div className="content">
-            <div>
-        <strong>{comment.user.name}</strong>
-        <p key={comment._id.toString()}> {comment.text} </p>
-        {isOwner(comment.user._id) &&
-        <button className="button" comment-id={comment._id}
-        onClick={this.commentHandleDelete}>Delete
-        </button>}
+    try {
+      const commentId = event.target.getAttribute('comment-id')
+      const plantId = this.state.plant._id
+      await deleteComment(plantId, commentId)
+    } catch (err) {
+      console.log(err)
+    }
+    this.getData()
+  }
+
+  render() {
+    if (!this.state.plant) return null
+    const { plant, text } = this.state //* text field in state
+
+    return (
+      <div className="media-content">
+        <form onSubmit={this.commentHandleSubmit}>
+          <div className="field">
+            <p className="control">
+              <textarea
+                className="textarea"
+                placeholder="Enter your comment here"
+                name="text"
+                onChange={this.commentHandleChange}
+                value={text}
+              />
+            </p>
+          </div>
+          <div className="field">
+            <p className="control">
+              <button type="submit" className="button">Post Comment</button>
+            </p>
+            <br />
+          </div>
+        </form>
+        <div>
+          <article className="media">
+            <div className="media-content">
+              <div className="content">
+                {plant.comments.map((comment, index) => {
+                  return (
+                    <div className="item" key={index}>
+                      <p>
+                      <strong>{comment.user.name}</strong>
+                      </p>
+                      {/* <br/> */}
+                      <p> {comment.text} </p>
+                      {/* <br/> */}
+                      {isOwner(comment.user._id) &&
+                        <button className="delete comment-delete-button" comment-id={comment._id}
+                          onClick={this.commentHandleDelete}>Delete
+                            </button>}
+                            <hr/>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          </article>
         </div>
-        </div>
-        </div>
-        </article>
-      )
-    })}
-    </div>
-</div>
-  )
-}
+      </div>
+    )
+  }
 
 }
 
