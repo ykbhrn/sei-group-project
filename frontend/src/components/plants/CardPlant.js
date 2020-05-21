fbe823a310f9fd840d6db0735cbfc76536d24135
George Jones <george@circleaudiovisual.com>
2020-05-21 09:37:54 +0100

    tried but failed to make height more intelligent

--- frontend/src/components/plants/CardPlant.js
+++ frontend/src/components/plants/CardPlant.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Likes from '../common/Likes'
 
-const CardPlant = ({ name, imageUrl, _id, likes }) => (
+const CardPlant = ({ nickName, name, imageUrl, _id, likes }) => (
   <div className="column column is-one-quarter-desktop is-one-third-tablet is-8-mobile is-offset-2-mobile">
     {/* { To produce a correct address to navigate to the correct plants show page, we concatenate the id prop passed down to plant into the Link components "to" prop } */}
     <div className="card">
       <Link to={`/plants/${_id}`}>
     <div>
       <div className="card-header">
-        <h4 className="card-header-title">{name}</h4>
+<h4 className="card-header-title">{nickName ? `${nickName} the ` : ''}{name}</h4>
       </div>
       <div className="card-image">
         <figure className="image image is-1by1">
